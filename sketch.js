14eaa61602a72ee6966abe2cd6e25ae3dc079718
Daniella Gáti <48113417+dgati@users.noreply.github.com>
2025-12-01 12:43:16 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -26,12 +26,17 @@ function draw() {
   // with the invert filter applied.
   image(capture, 0, 0, 360, 400);
 
-  // Experiment with the following filters to apply to the video!
+  // Experiment with applying filters  to the video!
+  filter(INVERT);
+
+  // here are some filter options to play with: 
   // INVERT - what does this do?
-  //  THRESHOLD - Pixels with a grayscale value above a given threshold are converted to white. The rest are converted to black. The threshold must be between 0.0 (black) and 1.0 (white).
-  // Limits the number of colors in the image. Each color channel is limited to the number of colors specified. Values between 2 and 255 are valid, but results are most noticeable with lower values. The default value is 4.
-  // filter(INVERT);
+  // THRESHOLD - Pixels with a grayscale value above a given threshold are converted to white. 
+  // The rest are converted to black. The threshold must be between 0.0 (black) and 1.0 (white).
+  // POSTERIZE -Limits the number of colors in the image. Each color channel is limited to the number of colors specified. 
+  // Values between 2 and 255 are valid, but results are most noticeable with lower values. The default value is 4.
+   
   // filter(THRESHOLD, 0.7);
-  filter(POSTERIZE, 2.5);
+  // filter(POSTERIZE, 2.5);
   
 }
